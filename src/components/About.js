330d7d17e0328a32b68bca7a5b11402b2bfa76a4
Moorindal <moorindal@ymail.com>
2019-03-29 09:38:08 -0500

    Put the app version on About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Screen from './Screen';
-import {Text, View, WebView} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import {
   Container,
   Content,
@@ -8,6 +8,7 @@ import {
 } from 'native-base';
 import LogoTitle from 'app/src/elements/LogoTitle';
 import {styles, dP} from 'app/utils/style/styles';
+import { expo } from 'app/app.json';
 
 export default class About extends Screen {
   constructor(props) {
@@ -20,18 +21,19 @@ export default class About extends Screen {
   };
 
   render() {
+    const {version} = expo;
     return (
       <Container style={{backgroundColor: dP.color.primary}}>
-        <Content padder style={{ width: '100%', padding:24}}>
-          <Body>
-            <Text style={{...styles.whiteTextColor, marginBottom: 8, textAlign: 'justify'}}>
-              В июне 2018 года под брендом Easy4 начал работать новый виртуальный оператор связи. Easy4 использует интеллектуальную платформу Multi IMSI* от резидента фонда «Сколково». Это позволяет создавать уникальные продукты как в сфере туризма для путешественников, так и сложные технические решения для корпоративных клиентов в различных секторах экономики. Easy4 может эффективно использовать сети других операторов связи, подключаться к российским и международным сетям по принципу лучшего качества сигнала и, таким образом, обеспечивать надежное качество связи.
+        <View style={{ height: '100%', padding:24, justifyContent: 'center'}}>
+          <View style={{alignItems: 'center'}}>
+            <Image
+              source={require('app/assets/image/logo.png')}
+            />
+            <Text style={{...styles.whiteTextColor, marginTop: 24, textAlign: 'justify'}}>
+              {version}
             </Text>
-            <Text style={{...styles.whiteTextColor, marginBottom: 8, textAlign: 'justify'}}>
-              Скачайте мобильное приложение Easy4 для ознакомления с тарифами оператора, контроля баланса и оперативного пополнения лицевого счета.
-            </Text>
-          </Body>
-        </Content>
+          </View>
+        </View>
       </Container>
     );
   }
