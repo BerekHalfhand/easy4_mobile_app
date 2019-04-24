66dc211df4976e217b61dcfd380e2439fb9ed362
Moorindal <moorindal@ymail.com>
2019-04-24 05:52:00 -0500

    Y.Drobkov: task 3349: adaptive font size

--- src/elements/ClientMainBalance.js
+++ src/elements/ClientMainBalance.js
@@ -2,22 +2,23 @@ import React from 'react';
 import { View } from 'native-base';
 import { Text, Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
+import {scale} from 'app/utils/scaling';
 
 export default class ClientMainBalance extends React.Component{
   constructor(props){
     super(props);
   }
   render(){
-    const { width } = Dimensions.get('window');
-    const guidelineBaseWidth = 420;
-    const scale =  width / guidelineBaseWidth;
-
     return(
       <View style={{justifyContent: 'flex-end'}}>
         <Text style={{fontFamily:'Roboto_light', fontSize:18, color:'#FFFFFF'}}>
           баланс
         </Text>
-        <Text style={{fontFamily:'Roboto_black', fontSize:(32*scale), color:'#FFFFFF'}}>
+        <Text
+          adjustsFontSizeToFit
+          numberOfLines={1}
+          style={{fontFamily:'Roboto_black', fontSize: scale(26), color:'#FFFFFF'}}
+        >
           {this.props.balance} ₽
         </Text>
       </View>
