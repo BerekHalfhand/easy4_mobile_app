e364970f7e67ba4093bc7792a23a4873ee7d4c5d
Moorindal <moorindal@ymail.com>
2019-05-17 10:01:40 -0500

    [develop] missing code fix

--- utils/scaling.js
+++ utils/scaling.js
@@ -9,4 +9,14 @@ const scale = size => width / guidelineBaseWidth * size;
 const verticalScale = size => height / guidelineBaseHeight * size;
 const moderateScale = (size, factor = 0.5) => size + ( scale(size) - size ) * factor;
 
-export {scale, verticalScale, moderateScale};
+const scaleTextToFit = (baseSize, containerWidthRatio, text, lengthOverride) => {
+  // fontSize = Math.sqrt(textWidth*textHeight/text.length)
+  // containerWidthRatio - float 0..1, where 1 = 100% screen width
+  const textWidth = scale(baseSize);
+  const textHeight = containerWidthRatio * width;
+  const length = lengthOverride || text.toString().length;
+
+  return Math.sqrt(textWidth*textHeight/length);
+};
+
+export {scale, verticalScale, moderateScale, scaleTextToFit};
