87f01f40c6b83061d2b406cf807fce43c2722dd4
Evgeniy <e.scherbak@easy4.pro>
2019-08-07 01:18:15 +0300

    v1.1.23

--- src/elements/Drawer.js
+++ src/elements/Drawer.js
@@ -132,6 +132,15 @@ class Drawer extends React.Component {
       </TouchableOpacity>
     ) : null );
 
+    const changePassword = (accessToken ? (
+        <TouchableOpacity style={styles.itemStyle} onPress={() => this.navigateTo('Profile')}>
+          <View style={styles.icon}>
+            <MaterialCommunityIcons name='information' color={dP.color.primary} size={24} />
+          </View>
+          <Text style={styles.itemText}>Смена пароля</Text>
+        </TouchableOpacity>
+    ) : null );
+
     return (
       <Container>
         <TouchableHighlight style={styles.headerContainer} onPress={() => (accessToken ? this.navigateTo('Main') : null)}>
@@ -142,12 +151,7 @@ class Drawer extends React.Component {
         </TouchableHighlight>
         <View style={styles.itemsContainer}>
           {selectedPhone ? increaseBalance : null}
-          <TouchableOpacity style={styles.itemStyle} onPress={() => this.navigateTo('Profile')}>
-            <View style={styles.icon}>
-              <MaterialCommunityIcons name='account-circle' color={dP.color.primary} size={24} />
-            </View>
-            <Text style={styles.itemText}>Смена пароля</Text>
-          </TouchableOpacity>
+          {changePassword}
           <TouchableOpacity style={styles.itemStyle} onPress={() => this.navigateTo('Contacts')}>
             <View style={styles.icon}>
               <MaterialCommunityIcons name='account-circle' color={dP.color.primary} size={24} />
